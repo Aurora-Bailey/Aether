44fead8a8b161dea29d912149e6c37b93b82b36c
Brad Thompson <bradleyglind@gmail.com>
2016-04-14 03:32:15 -0600

    Added up and down arrow key scrolling

--- public/es6/events.js
+++ public/es6/events.js
@@ -2,10 +2,51 @@
 $('#aether-nav').find('form').on('submit', function(){
     var subreddit = $('#aether-subreddit').val();
     Reddit.getSubreddit(subreddit);
+    $('html, body').animate({
+        scrollTop: '0px'
+    }, 'fast');
     return false;
 }).trigger('submit');
 
 $('#aether-load-more').on('click', function(){
     Reddit.getSubreddit(null);
     return false;
+});
+
+$(window).on('keydown', function(e){
+    if(e.keyCode == 40){
+        e.preventDefault();
+
+        let $next = false;
+        let current_scroll = document.body.scrollTop;
+        $('#aether-content').find('.item').each(function(index, value){
+            if($(value).offset().top > current_scroll + 40 && $next === false){
+                $next = $(value);
+            }
+
+        });
+        if($next !== false){
+            $('html, body').animate({
+                scrollTop: ($next.offset().top - 40) + 'px'
+            }, 'fast');
+        }
+
+    }
+    if(e.keyCode == 38){
+        e.preventDefault();
+
+        let $next = false;
+        let current_scroll = document.body.scrollTop;
+        $($('#aether-content').find('.item').get().reverse()).each(function(index, value){
+            if($(value).offset().top < current_scroll + 40 && $next === false){
+                $next = $(value);
+            }
+
+        });
+        if($next !== false){
+            $('html, body').animate({
+                scrollTop: ($next.offset().top - 40) + 'px'
+            }, 'fast');
+        }
+    }
 });
