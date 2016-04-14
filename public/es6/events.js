00cb38ce58625bc32c5c20ee89d99da053ec043b
Brad Thompson <bradleyglind@gmail.com>
2016-04-13 23:45:32 -0600

    Core functionality reddit and imgur api basics

--- public/es6/events.js
+++ public/es6/events.js
@@ -1,1 +1,68 @@
 // Events
+/*
+$.getJSON('https://www.reddit.com/r/all.json', function(response){
+    var posts = response.data.children;
+
+    posts.forEach(function(p){
+        var thumbnail = p.data.thumbnail;
+        var domain = p.data.domain;
+        var title = p.data.title;
+        var url = p.data.url;
+        var ext = url.split('.').pop();
+
+        $("body").append(title)
+            .append('<br>')
+            .append(url)
+            .append('<br>')
+            .append('<img src="' + thumbnail + '">')
+            .append('<br>')
+            .append('<br>');
+
+        if(ext == 'jpg' || ext == 'gif' || ext == 'png' || ext == 'webp'){
+            console.log('Native image: ', ext, ' == ', url);
+
+        }else if(domain == 'imgur.com' || domain == 'i.imgur.com'){
+            console.log('imgur: ', url);
+
+            var imgur_id = url.split('/').pop()
+
+
+            if(url.match(/^https?:\/\/i?\.?imgur\.com\/a\/[a-z0-9]+$/gi)){// /a/{id}
+                console.log('get album: ', 'https://api.imgur.com/3/album/' + imgur_id);
+                $.getJSON('https://api.imgur.com/3/album/' + imgur_id, function(response){
+                    console.log(response);
+                });
+            }else if(url.match(/^https?:\/\/i?\.?imgur\.com\/[a-z0-9]+$/gi)){// /{id}
+                var img_url = url.replace('/imgur', '/i.imgur') + '.jpg';
+                console.log(img_url);
+                $("body").append('<img src="' + img_url + '">')
+                    .append('<br>')
+                    .append('<br>');
+            }else if(url.match(/^https?:\/\/i?\.?imgur\.com\/[a-z0-9]+\.jpg$/gi)){// /{id}.jpg
+                var img_url = url.replace('/imgur', '/i.imgur');
+                console.log(img_url);
+                $("body").append('<img src="' + img_url + '">')
+                    .append('<br>')
+                    .append('<br>');
+            }
+        }
+
+    });
+
+
+
+    console.log(response);
+    console.log(posts);
+});
+*/
+
+$('#aether-nav').find('form').on('submit', function(){
+    var subreddit = $('#aether-subreddit').val();
+    Reddit.getSubreddit(subreddit);
+    return false;
+}).trigger('submit');
+
+$('#aether-load-more').on('click', function(){
+    Reddit.getSubreddit(null);
+    return false;
+});
--- public/es6/lib.js
+++ public/es6/lib.js
@@ -88,4 +88,154 @@ class Lib {
         else if(site == 'twitter')
             window.open('http://twitter.com/intent/tweet?status=' + $(document).find("title").text() + '+' + url, 'Twitter', 'width=550,height=400');
     }
+
+    static extractDomain(url) {
+        var domain;
+
+        //find & remove protocol (http, ftp, etc.) and get domain
+        if (url.indexOf("://") > -1)
+            domain = url.split('/')[2];
+        else
+            domain = url.split('/')[0];
+
+        //find & remove port number
+        domain = domain.split(':')[0];
+        return domain;
+    }
+
+    static resolveLinkMedia(link, callback){
+        var ext = link.split('.').pop();
+        var domain = this.extractDomain(link);
+
+        if(ext == 'jpg' || ext == 'gif' || ext == 'png' || ext == 'webp'){
+            // Link IS an image
+            callback({type: 'image', data: [{link: link}]});
+
+        }else if(domain == 'imgur.com' || domain == 'i.imgur.com'){
+            var imgur_id = link.split('/').pop();
+            var imgur_api = 'https://api.imgur.com/3';
+            var imgur_auth = 'Client-ID 1a264d08fe7cd9c';
+
+            if(link.match(/^https?:\/\/i?\.?imgur\.com\/a\/[a-z0-9]+$/gi)){// /a/{id} ALBUM
+                $.ajax({
+                    url: imgur_api + '/album/' + imgur_id,
+                    type: 'GET',
+                    dataType: 'json',
+                    success: function(result){
+                        callback({type: 'imgur', data: result.data.images});
+                    }
+                });
+
+            }else if(link.match(/^https?:\/\/i?\.?imgur\.com\/[a-z0-9]+$/gi)){// /{id} ROOT
+                $.ajax({
+                    url: imgur_api + '/image/' + imgur_id,
+                    type: 'GET',
+                    dataType: 'json',
+                    success: function(result){
+                        callback({type: 'imgur', data: [result.data]});
+                    }
+                });
+
+            }else if(ext == 'gifv'){// {id}.gifv GIFV
+                imgur_id = imgur_id.split('.')[0];// remove .gifv
+                $.ajax({
+                    url: imgur_api + '/image/' + imgur_id,
+                    type: 'GET',
+                    dataType: 'json',
+                    success: function(result){
+                        callback({type: 'imgur', data: [result.data]});
+                    }
+                });
+            }
+        }
+
+
+    }
 }
+
+
+class Reddit {
+    static init(){
+        this.$container = $('#aether-content');
+        this.$template = $('#aether-templates').find('.item');
+        this.subreddit = null;
+        this.page = false; // for paging
+        this.processing = false;
+    }
+
+    static getSubreddit(subreddit){// null for concat
+        this.processing = true;
+        if(subreddit !== null){
+            this.subreddit = subreddit;
+            this.page = false;
+        }
+
+        this.fetchSubreddit((results)=>{
+            this.buildSubreddit(results.data.children, this.page === false);
+            this.page = results.data.after;
+        });
+    }
+
+    static buildSubreddit(posts, fresh){
+        if(fresh)
+            this.$container.html('');
+
+        posts.forEach((p)=>{
+            var {url, permalink, score, thumbnail, title, author, num_comments, saved, subreddit, domain} = p.data;
+            var reddit_url = 'https://reddit.com' + permalink;
+
+            var $item = this.$template.clone();
+            $item.find('.title').html(title);
+            $item.find('.subreddit').html(subreddit);
+            $item.find('.author').html(author);
+            $item.find('.score').html(score);
+            $item.find('.link-reddit').html('<a target="_blank" href="' + reddit_url + '">view on reddit (' + num_comments + ' comments)</a>');
+            $item.find('.link-url').html('<a target="_blank" href="' + url + '">origin link - '+ domain +'</a>');
+
+            $item.addClass('hide');
+            Lib.resolveLinkMedia(url, function(media){
+                if(media.type == 'image'){
+                    $item.find('.aether-media').append('<img src="' + media.data[0].link + '">');
+                }else if(media.type == 'imgur'){
+                    media.data.forEach(function(image){
+                        if(image.animated == false){
+                            $item.find('.aether-media').append('<img src="' + image.link + '">');
+                        }else{
+                            var $vid = $('<video autoplay="" loop="" muted="" preload="" class="aether-gifv"></video>');
+                            $vid.append($('<source src="' + image.webm + '" type="video/webm" class="aether-gifv-src-webm">'));
+                            $vid.append($('<source src="' + image.mp4 + '" type="video/mp4" class="aether-gifv-src-mp4">'));
+                            $item.find('.aether-media').append($vid);
+                        }
+                    });
+                    console.log(media.data);
+                }
+
+                $item.removeClass('hide');
+            });
+
+            this.$container.append($item);
+        });
+    }
+
+    static fetchSubreddit(callback){
+        if(this.subreddit === null)
+            return false;
+
+        var url = 'https://www.reddit.com/r/' + this.subreddit + '/.json';
+
+        if(this.page !== false)
+            url += '?count=25&after=' + this.page;
+
+        $.getJSON(url, function(response){
+            callback(response);
+        });
+    }
+}
+Reddit.init();
+
+
+
+
+
+
+
